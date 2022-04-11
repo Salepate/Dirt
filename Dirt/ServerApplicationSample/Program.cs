1fe7c64d32ceb5f23d439b3546a9073d1061420b
Salepate <salepate@gmail.com>
2022-04-11 20:58:49 +0200

    add dirt server sample

---
+++ Dirt/ServerApplicationSample/Program.cs
@@ -0,0 +1,13 @@
+﻿using Dirt.ServerApplication;
+
+namespace Dirt.ServerApplicationSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerApp app = new ServerApp();
+            app.Run();
+        }
+    }
+}
