70b19be14c636062b9bcafa41345f41b8cdacb45
Salepate <salepate@gmail.com>
2022-04-13 15:17:55 +0200

    provide custom enum entry for plugin netops

--- Mud/Mud/MudOperation.cs
+++ Mud/Mud/MudOperation.cs
@@ -2,11 +2,12 @@
 {
     public enum MudOperation
     {
-        Ping            = 0,
-        ClientAuth      = 1,
-        ValidateAuth    = 2,
-        Disconnect      = 3,
-        MultiPackets    = 4,
+        Ping             = 0,
+        ClientAuth       = 1,
+        ValidateAuth     = 2,
+        Disconnect       = 3,
+        MultiPackets     = 4,
+        CustomOperation0 = 20,
         Error           = 255
     }
 }
