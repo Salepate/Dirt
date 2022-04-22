8f2006cddc607563caabc15cf2975d5ca436a8c1
Salepate <salepate@gmail.com>
2022-04-22 13:31:32 +0200

    add toFloat2()/toVector2() extensions methods

--- Unity/Common/Dirt/Simulation/UnityExtension.cs
+++ Unity/Common/Dirt/Simulation/UnityExtension.cs
@@ -5,6 +5,8 @@ namespace Dirt.Simulation
     public static class UnityExtension
     {
         public static UnityEngine.Vector3 toVector(this float3 p) { return new UnityEngine.Vector3(p.x, p.y, p.z); }
+        public static UnityEngine.Vector2 toVector(this float2 p) { return new UnityEngine.Vector2(p.x, p.y); }
         public static float3 toFloat3(this UnityEngine.Vector3 v) { return new float3(v.x, v.y, v.z); }
+        public static float2 toFloat2(this UnityEngine.Vector2 v) { return new float2(v.x, v.y); }
     }
 }
