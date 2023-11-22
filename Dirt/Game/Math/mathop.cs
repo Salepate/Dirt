24deb2646b659979735fc33af5a0f8bcdfbaf592
Salepate <salepate@gmail.com>
2023-11-22 12:47:24 +0100

    add mathop.min(int,int)  / mathop.max(int,int)

--- Dirt/Game/Math/mathop.cs
+++ Dirt/Game/Math/mathop.cs
@@ -5,6 +5,18 @@ namespace Dirt.Game.Math
     public static class mathop
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int min(int a, int b)
+        {
+            return a <= b ? a : b;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float min(float a, float b)
         {
             return a <= b ? a : b;
