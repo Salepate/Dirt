a9adb03624d7b5bb779f8ac187a259154d77bbaa
Salepate <salepate@gmail.com>
2024-03-24 19:07:10 +0100

    add missing files

---
+++ Dirt/Game/Container/SwapTable.cs
@@ -0,0 +1,43 @@
+﻿namespace Dirt.Game.Container
+{
+    public class SwapTable<T>
+    {
+        public T[] m_Table;
+        public int Count { get; private set; }
+        public SwapTable(int size)
+        {
+            m_Table = new T[size];
+            Count = 0;
+        }
+
+        // overload the [] operator
+        public ref T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Count)
+                    throw new System.Exception("index out of range");
+                return ref m_Table[index];
+            }
+        }
+
+        public void Add(in T value)
+        {
+            if (Count < m_Table.Length)
+            {
+                m_Table[Count] = value;
+                ++Count;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (Count > 0 && index >= 0 && index < Count)
+            {
+                if (index != Count - 1)
+                    m_Table[index] = m_Table[Count - 1];
+                --Count;
+            }
+        }
+    }
+}
---
+++ Dirt/Game/Math/RNGLite.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Dirt.Game.Math
+{
+    /// <summary>
+    /// Based on https://learn.microsoft.com/en-us/archive/msdn-magazine/2016/august/test-run-lightweight-random-number-generation
+    /// </summary>
+    public class RNGLite
+    {
+        private const long A = 25214903917;
+        private const long B = 11;
+        private long m_Seed;
+        public RNGLite(long seed = 0)
+        {
+            SetSeed(seed);
+        }
+
+        public void SetSeed(long seed)
+        {
+            if (seed < 0)
+            {
+                seed = 0;
+            }
+
+            m_Seed = seed;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="min">min (inclusive)</param>
+        /// <param name="max">max (inclusive)</param>
+        /// <returns></returns>
+        public int Range(int min, int max) 
+        {
+            return min + NextInt(31) % (max - min + 1);
+        }
+        private int NextInt(int bits) // helper
+        {
+            m_Seed = (m_Seed * A + B) & ((1L << 48) - 1);
+            return (int)(m_Seed >> (48 - bits));
+        }
+        public float Next()
+        {
+            return (((long)NextInt(26) << 27) + NextInt(27)) / (float)(1L << 53);
+        }
+    }
+}
