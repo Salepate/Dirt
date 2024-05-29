52d1e2c2618346d3e26f313013662d70d9980ef6
Salepate <salepate@gmail.com>
2024-05-29 15:35:46 +0200

    add clamp

--- Dirt/Game/Math/mathop.cs
+++ Dirt/Game/Math/mathop.cs
@@ -17,6 +17,12 @@ namespace Dirt.Game.Math
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int clamp(int value, int min, int max)
+        {
+            return value < min ? min : value > max ? max : value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float min(float a, float b)
         {
             return a <= b ? a : b;
