54bd1bc417907dee3cba8ff607f8efe60c4e751a
Salepate <salepate@gmail.com>
2024-09-03 18:43:45 +0200

    add min/max for ulong and long

--- Dirt/Game/Math/mathop.cs
+++ Dirt/Game/Math/mathop.cs
@@ -4,6 +4,8 @@ namespace Dirt.Game.Math
 {
     public static class mathop
     {
+
+        #region Integer
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int min(int a, int b)
         {
@@ -21,7 +23,10 @@ namespace Dirt.Game.Math
         {
             return value < min ? min : value > max ? max : value;
         }
+        #endregion
 
+
+        #region Float
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float min(float a, float b)
         {
@@ -39,5 +44,33 @@ namespace Dirt.Game.Math
         {
             return a < 0 ? -a : a;
         }
+        #endregion
+
+        #region Signed Long
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static long min(long a, long b)
+        {
+            return a <= b ? a : b;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static long max(long a, long b)
+        {
+            return a > b ? a : b;
+        }
+        #endregion
+        #region Unsigned Long
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ulong min(ulong a, ulong b)
+        {
+            return a <= b ? a : b;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ulong max(ulong a, ulong b)
+        {
+            return a > b ? a : b;
+        }
+        #endregion
     }
 }
