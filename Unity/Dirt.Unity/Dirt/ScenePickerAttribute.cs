0d466aef3043fe65059ae20650aa810c776f3b66
Salepate <salepate@gmail.com>
2025-07-30 12:33:40 +0200

    add scene picker attribute

---
+++ Unity/Dirt.Unity/Dirt/ScenePickerAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Dirt.Unity.Dirt
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class ScenePickerAttribute : UnityEngine.PropertyAttribute
+    {
+        /// <summary>
+        /// Include scene from player build only
+        /// </summary>
+        public bool Build;
+        /// <summary>
+        /// Supports *
+        /// </summary>
+        public string Pattern = string.Empty;
+
+        public ScenePickerAttribute(string pattern, bool buildOnly = true)
+        {
+            Build = buildOnly;
+            Pattern = pattern;
+        }
+    }
+}
