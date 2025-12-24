381f725092e4aa091290c8c70f5ecdab1df3ff16
Salepate <salepate@gmail.com>
2025-12-24 11:25:04 +0100

    add enum utility

---
+++ Dirt/Game/Utils/EnumUtility.cs
@@ -0,0 +1,25 @@
+﻿namespace Dirt.Game.Utils
+{
+    public static class EnumUtility
+    {
+        public static int GetHighestValue<T>() where T : System.Enum
+        {
+            var values = typeof(T).GetEnumValues();
+            int max = 0;
+            for (int i = 0; i < values.Length; ++i)
+            {
+                object genValue = values.GetValue(i);
+                int enumV = System.Convert.ToInt32(genValue);
+
+                if (enumV > max)
+                    max = enumV;
+            }
+            return max;
+        }
+        public static T[] CreateArray<T, C>() where C : System.Enum
+        {
+            int size = GetHighestValue<C>();
+            return new T[size + 1];
+        }
+    }
+}
