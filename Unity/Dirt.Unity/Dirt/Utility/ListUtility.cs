20988fa940323b370e6c4f951cdf3a7498e6cc31
Salepate <salepate@gmail.com>
2026-01-01 19:35:52 +0100

    add list utility

---
+++ Unity/Dirt.Unity/Dirt/Utility/ListUtility.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dirt.Utility
+{
+    public static class ListUtility
+    {
+        public static void DestroyList<T>(this IList<T> list) where T : Component
+        {
+            for (int i = 0; i < list.Count; ++i)
+            {
+                GameObject.Destroy(list[i].gameObject);
+            }
+            list.Clear();
+        }
+    }
+}
